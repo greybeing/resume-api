98def70192ddf5a87f9492c017b41a8f18ea6ad7
greybeing <adulojugbenga@yahoo.com>
2022-12-23 17:28:36 +0100

    .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { DownloadController } from './controllers/download.controller';
 import { DownloadService } from './services/download.service';
+import { ConfigModule } from 'nestjs-dotenv';
+import { ConfigService } from 'nestjs-dotenv';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController, DownloadController],
   providers: [AppService, DownloadService],
 })
-export class AppModule {}
+export class AppModule {
+  static port: string
+  constructor(configService: ConfigService) {
+    AppModule.port = configService.get('PORT')
+  }
+}
--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from 'nestjs-dotenv';
+
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Oluwagbenga.com-API';
   }
+  constructor(
+    private readonly configService: ConfigService
+) {}
 }
+
+
