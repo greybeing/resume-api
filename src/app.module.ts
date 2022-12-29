82af03fd84bd91b203a38537cbe9a8fb8690b868
greybeing <adulojugbenga@yahoo.com>
2022-12-29 15:21:23 +0100

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { DownloadController } from './controllers/download.controller';
 import { DownloadService } from './services/download.service';
-import { ConfigModule } from 'nestjs-dotenv';
-import { ConfigService } from 'nestjs-dotenv';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot({  envFilePath: 'src/.env'   })
   ],
   controllers: [AppController, DownloadController],
   providers: [AppService, DownloadService],
